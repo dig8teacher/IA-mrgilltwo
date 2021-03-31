a2c5014e840522fc7db2d4282d3a3d47739beea7
dig8teacher <jgill@sd42.ca>
2021-03-31 10:27:44 -0700

    elven

--- variableone.java
+++ variableone.java
@@ -4,6 +4,7 @@ class variableone {
 		int number1 = 22;
 		int number2 = 22;
 		int number3 = 22;
-		System.out.println(number1+number2+number3);
+		int number4 = 22;
+		System.out.println(number1+number2+number3+number4);
 	}
 }
